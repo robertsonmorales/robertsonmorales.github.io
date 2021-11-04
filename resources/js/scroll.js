8cd31bef86e53fe330cfed1934815618173d9db9
Robertson Morales <moralesso152842@gmail.com>
2021-11-04 23:55:37 +0800

    added scroll reveal animations

---
+++ resources/js/scroll.js
@@ -0,0 +1,43 @@
+ScrollReveal().reveal('nav', {
+    delay: 100,
+    distance: '5%',
+    origin: 'top',
+    easing: 'ease'
+});
+
+ScrollReveal().reveal('#catch-phrase', {
+    delay: 100,
+    distance: '5%',
+    origin: 'right',
+    easing: 'ease'
+});
+
+ScrollReveal().reveal('#specialize', {
+    delay: 200,
+    distance: '5%',
+    origin: 'left',
+    easing: 'ease'
+});
+
+// CAll TO ACTIONS
+ScrollReveal().reveal('#btn-my-works', {
+    delay: 300,
+    distance: '15%',
+    origin: 'right',
+    easing: 'ease'
+});
+
+ScrollReveal().reveal('#btn-download-resume', {
+    delay: 300,
+    distance: '15%',
+    origin: 'left',
+    easing: 'ease'
+});
+
+ScrollReveal().reveal('#avatar', {
+    delay: 100,
+    duration: 900,
+    distance: '10%',
+    origin: 'top',
+    easing: 'ease'
+});
