262bf90f79ec69cd746fd6efc994f3595a9ae1dd
Robertson Morales <moralesso152842@gmail.com>
2022-07-10 16:32:54 +0800

    updated facebook plugin

--- index.php
+++ index.php
@@ -63,36 +63,36 @@
     </script>
 </head>
 <body>
-    <!-- Messenger Chat Plugin Code -->
+
+    <!-- Messenger Chat plugin Code -->
     <div id="fb-root"></div>
 
-    <!-- Your Chat Plugin code -->
-    <div id="fb-customer-chat"
-        page_id="105844545345201"
-        attribution="biz_inbox"
-        class="fb-customerchat">
+    <!-- Your Chat plugin code -->
+    <div id="fb-customer-chat" class="fb-customerchat">
     </div>
 
     <script>
-      var chatbox = document.getElementById('fb-customer-chat');
+    var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "105844545345201");
+    chatbox.setAttribute("attribution", "biz_inbox");
     </script>
 
     <!-- Your SDK code -->
     <script>
-      window.fbAsyncInit = function() {
+    window.fbAsyncInit = function() {
         FB.init({
-          xfbml            : true,
-          version          : 'v12.0'
+        xfbml            : true,
+        version          : 'v14.0'
         });
-      };
+    };
 
-      (function(d, s, id) {
+    (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
         js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
         fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
+    }(document, 'script', 'facebook-jssdk'));
     </script>
 
     <header>
