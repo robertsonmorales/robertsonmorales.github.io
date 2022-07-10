a2c55595a339762d8d36bdffac46645dce293046
Robertson Morales <moralesso152842@gmail.com>
2022-07-10 13:52:17 +0800

    added new contact me

--- resources/js/contact.js
+++ resources/js/contact.js
@@ -1,34 +1,100 @@
 $(() => {
   'use strict';
 
+  var htmlContent = '';
+  var offeredService = $('.card-title');
+  for (let i = 0; i < offeredService.length; i++) {
+    var services = offeredService[i].textContent;
+    var elName = services.split(' ').join('_').toLowerCase();
+
+    htmlContent += '<div class="custom-control custom-checkbox">\
+      <input type="checkbox" name="services[]" class="custom-control-input" id="'+elName+'" value="'+services+'">\
+      <label class="custom-control-label" for="'+elName+'">'+services+'</label>\
+    </div>';
+  }
+
+  $('#offered-services').prepend(htmlContent);
+
   $('#contact-form').on('submit', function(event){
     event.preventDefault();
+    
+    var counter = [];
+    var myServices = $('input[name="services[]"]');
+    for (let j = 0; j < myServices.length; j++) {
+      var isChecked = myServices[j].checked;
+      counter.push(isChecked);
+    }
 
-    let name = isEmpty($('#name').val(), $('#name').attr('id'), $('#name').attr('name'));
+    var noSelectedService = counter.find((el) => el == true);
+    if(noSelectedService){
+      
+      $('#validate-subject').text("").removeClass('d-block');
+    }else{
+      $('#validate-subject').text("Please select at least one.").addClass('d-block');
+    }
+
+    let firstName = isEmpty($('#first_name').val(), $('#first_name').attr('id'), $('#first_name').attr('name'));
+    let lastName = isEmpty($('#last_name').val(), $('#last_name').attr('id'), $('#last_name').attr('name'));
     let emailAddress = isEmpty($('#email_address').val(), $('#email_address').attr('id'), $('#email_address').attr('name'));
-    let subject = isEmpty($('#subject').val(), $('#subject').attr('id'), $('#subject').attr('name'));
+    let phoneNumber = isEmpty($('#phone_number').val(), $('#phone_number').attr('id'), $('#phone_number').attr('name'));
     let message = isEmpty($('#message').val(), $('#message').attr('id'), $('#message').attr('name'));
     let validateEmailAddress = validateEmail($('#email_address').val());
 
-    if(!name && !emailAddress && !subject && !message && !validateEmailAddress){
-      // $('#btn-submit').prop('disabled', true);
-      // $('#btn-submit').css('cursor', 'not-allowed');
-      // $('#btn-submit .mr-2').html('SENDING..');
-
-      $('.trigger-email').attr('href', 'mailto:'+ $('#my-email').text() +'?subject='+ $('#name').val() + ': ' + $('#subject').val() +'&body='+ $('#message').val().trim().replace(' ', '%20'));
-
-      window.open($('.trigger-email').attr('href'));
-
-      $('#contact-form .form-control').val("");
+    if(!firstName && !lastName && !phoneNumber && !emailAddress && !message && !validateEmailAddress){
+      ajaxRequest();
     }
   });
+
+  function ajaxRequest(){
+    var url = $('#contact-form').attr('action');
+    var httpMethod = $('#contact-form').attr('method');
+    var data = {
+      first_name: $('#first_name').val(),
+      last_name: $('#last_name').val(),
+      email_address: $('#email_address').val(),
+      phone_number: $('#phone_number').val(),
+      services: $('input[name="services[]"]').val(),
+      message: $('#message').val()
+    };
+    
+    $.ajax({
+      url: url,
+      type: httpMethod,
+      data: data,
+      beforeSend: () => {
+        $('#btn-submit').css('cursor', 'not-allowed')
+        .prop('disabled', true)
+        .html('Sending Message...');
+        $('.form-control').css('cursor', 'not-allowed')
+        .prop('disabled', true);
+        $('.custom-control-input').css('cursor', 'not-allowed')
+        .prop('disabled', true);
+      },
+      success: (result, status, xhr) => {
+        console.log(result);
+        if(xhr.status == 200 && status == "success"){ // Http OK
+          if(result.is_success){
+            // alert(result.message);
+            $('#contact-form .form-control').val("");
+            $('#contact-form .custom-control-input').attr("checked", false);
+          }else{
+            alert(result.message);
+          }
+        }
+      },
+      error: (xhr, status, error) => {
+        console.log('e-xhr', xhr);
+        console.log('e-status', status);
+        console.log('e-error', error);
+      }
+    });
+  }
 
   function isEmpty(input, id, name){
     let newName = name.replace('_', ' ');
     if  (input == "" || input == null || input == undefined){
       $('#'+id).addClass('is-invalid');
-      $('#validate-'+id).addClass('d-block');
-      $('#validate-'+id).html('The ' + newName + ' is a required field.');
+      $('#validate-'+id).text('The ' + newName + ' is a required field.').addClass('d-block');
     }else{
       $('#'+id).removeClass('is-invalid');
       $('#validate-'+id).removeClass('d-block');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a14fae29c24a27a7be8b6f1a4f4e7e76a0081210',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a14fae29c24a27a7be8b6f1a4f4e7e76a0081210',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.6.2',
+            'version' => '6.6.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'b52ed06864fdda81b82ec8bf564cf15d45ed4f95',
+            'dev_requirement' => false,
+        ),
+    ),
+);
