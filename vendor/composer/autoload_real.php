90434781f446db9a80fef386276e7fc54e6e34e6
robertsonmorales <58919933+robertsonmorales@users.noreply.github.com>
2023-02-01 15:27:12 +0000

    update: added skills

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,10 +22,8 @@ class ComposerAutoloaderInit807d56acd1d882e2ae6800bc9e47e18a
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit807d56acd1d882e2ae6800bc9e47e18a', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit807d56acd1d882e2ae6800bc9e47e18a', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,32 +1,33 @@
-<?php return array(
-    'root' => array(
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'type' => 'library',
-        'install_path' => __DIR__ . '/../../',
-        'aliases' => array(),
-        'reference' => 'a14fae29c24a27a7be8b6f1a4f4e7e76a0081210',
-        'name' => '__root__',
-        'dev' => true,
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
     ),
-    'versions' => array(
-        '__root__' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
-            'reference' => 'a14fae29c24a27a7be8b6f1a4f4e7e76a0081210',
-            'dev_requirement' => false,
-        ),
-        'phpmailer/phpmailer' => array(
-            'pretty_version' => 'v6.6.2',
-            'version' => '6.6.2.0',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
-            'aliases' => array(),
-            'reference' => 'b52ed06864fdda81b82ec8bf564cf15d45ed4f95',
-            'dev_requirement' => false,
-        ),
+    'reference' => '3ddfcad7d268084377e8e3c36f2692257a890a36',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3ddfcad7d268084377e8e3c36f2692257a890a36',
     ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.6.2',
+      'version' => '6.6.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b52ed06864fdda81b82ec8bf564cf15d45ed4f95',
+    ),
+  ),
 );
