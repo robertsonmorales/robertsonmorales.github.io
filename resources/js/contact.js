3d59a288310de685a0eafb0118878c403f0d314f
robertsonmorales <58919933+robertsonmorales@users.noreply.github.com>
2023-06-25 17:08:32 +0000

    updated contact form

--- resources/js/contact.js
+++ resources/js/contact.js
@@ -13,81 +13,21 @@ $(() => {
     </div>';
   }
 
-  $('#offered-services').prepend(htmlContent);
+  function getServices(){
+    let services = $('input[name="services[]"]');
+    let get = [];
 
-  $('#contact-form').on('submit', function(event){
-    event.preventDefault();
-    
-    var counter = [];
-    var myServices = $('input[name="services[]"]');
-    for (let j = 0; j < myServices.length; j++) {
-      var isChecked = myServices[j].checked;
-      counter.push(isChecked);
-    }
+    for (let i = 0; i < services.length; i++) {
+      const element = services[i];
 
-    var noSelectedService = counter.find((el) => el == true);
-    if(noSelectedService){
-      
-      $('#validate-subject').text("").removeClass('d-block');
-    }else{
-      $('#validate-subject').text("Please select at least one.").addClass('d-block');
-    }
-
-    let firstName = isEmpty($('#first_name').val(), $('#first_name').attr('id'), $('#first_name').attr('name'));
-    let lastName = isEmpty($('#last_name').val(), $('#last_name').attr('id'), $('#last_name').attr('name'));
-    let emailAddress = isEmpty($('#email_address').val(), $('#email_address').attr('id'), $('#email_address').attr('name'));
-    let phoneNumber = isEmpty($('#phone_number').val(), $('#phone_number').attr('id'), $('#phone_number').attr('name'));
-    let message = isEmpty($('#message').val(), $('#message').attr('id'), $('#message').attr('name'));
-    let validateEmailAddress = validateEmail($('#email_address').val());
-
-    if(!firstName && !lastName && !phoneNumber && !emailAddress && !message && !validateEmailAddress){
-      ajaxRequest();
-    }
-  });
-
-  function ajaxRequest(){
-    var url = $('#contact-form').attr('action');
-    var httpMethod = $('#contact-form').attr('method');
-    var data = {
-      first_name: $('#first_name').val(),
-      last_name: $('#last_name').val(),
-      email_address: $('#email_address').val(),
-      phone_number: $('#phone_number').val(),
-      services: $('input[name="services[]"]').val(),
-      message: $('#message').val()
-    };
-    
-    $.ajax({
-      url: url,
-      type: httpMethod,
-      data: data,
-      beforeSend: () => {
-        // $('#btn-submit').css('cursor', 'not-allowed')
-        // .prop('disabled', true)
-        // .html('Sending Message...');
-        // $('.form-control').css('cursor', 'not-allowed')
-        // .prop('disabled', true);
-        // $('.custom-control-input').css('cursor', 'not-allowed')
-        // .prop('disabled', true);
-      },
-      success: (result, status, xhr) => {
-        console.log(result);
-        if(xhr.status == 200 && status == "success"){ // Http OK
-          if(result.is_success){
-            alert(result.message);
-            $('#contact-form .form-control').val("");
-            $('#contact-form .custom-control-input').attr("checked", false);
-          }else{
-            alert(result.message);
-          }
-        }
-      },
-      error: (xhr, status, error) => {
-        console.log('e-xhr', xhr);
-        console.log('e-status', status);
-        console.log('e-error', error);
+      if(element.checked){
+        get.push(element.defaultValue)
       }
-    });
+    }
+
+    let list = get.join(', ');
+    
+    return list;
   }
 
   function isEmpty(input, id, name){
@@ -114,4 +54,92 @@ $(() => {
       return false;
     }
   }
+
+  function letsGetStarted(){
+    let url = $('#contact-form').attr('action');
+    let httpMethod = $('#contact-form').attr('method');
+    let data = {
+      FirstName: $('#first_name').val(),
+      LastName: $('#last_name').val(),
+      EmailAddress: $('#email_address').val(),
+      MobileNo: $('#phone_number').val(),
+      Services: getServices(),
+      Message: $('#message').val()
+    };
+
+    fetch(url, {
+      method: httpMethod,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then( (response) =>  {
+        $('#btn-submit')
+          .css('cursor', 'not-allowed')
+          .prop('disabled', true);
+        $('.form-control')
+          .css('cursor', 'not-allowed')
+          .prop('disabled', true);
+        $('.custom-control-input')
+          .css('cursor', 'not-allowed')
+          .prop('disabled', true);
+
+        $.getJSON("https://api.ipify.org?format=json", function(data) {
+          var ipAddress = data.ip;
+          sessionStorage.setItem('IP', ipAddress);
+        });
+
+        window.location.href = "/success.html";
+      })
+      .catch(function (error) {
+        window.location.href = "/error.html";
+      });
+  }
+
+  $('#offered-services').prepend(htmlContent);
+
+  $('#phone_number').on('input', function() {
+    let inputValue = $(this).val();
+    
+    if (inputValue.length <= 14) {
+      let numericValue = inputValue.replace(/[^0-9]/g, '');
+      $(this).val(numericValue);
+    } else {
+      let truncatedValue = inputValue.slice(0, 14);
+      let numericTruncatedValue = truncatedValue.replace(/[^0-9]/g, '');
+      $(this).val(numericTruncatedValue);
+    }
+  });
+
+  $('#btn-submit').click(function(event){
+    event.preventDefault();
+    
+    var counter = [];
+    var myServices = $('input[name="services[]"]');
+    for (let j = 0; j < myServices.length; j++) {
+      var isChecked = myServices[j].checked;
+      counter.push(isChecked);
+    }
+
+    var noSelectedService = counter.find((el) => el == true);
+    if(noSelectedService){
+      
+      $('#validate-subject').text("").removeClass('d-block');
+    }else{
+      $('#validate-subject').text("Please select at least one.").addClass('d-block');
+    }
+
+    let firstName = isEmpty($('#first_name').val(), $('#first_name').attr('id'), $('#first_name').attr('name'));
+    let lastName = isEmpty($('#last_name').val(), $('#last_name').attr('id'), $('#last_name').attr('name'));
+    let emailAddress = isEmpty($('#email_address').val(), $('#email_address').attr('id'), $('#email_address').attr('name'));
+    let phoneNumber = isEmpty($('#phone_number').val(), $('#phone_number').attr('id'), $('#phone_number').attr('name'));
+    let message = isEmpty($('#message').val(), $('#message').attr('id'), $('#message').attr('name'));
+    let validateEmailAddress = validateEmail($('#email_address').val());
+
+    if(!firstName && !lastName && !phoneNumber && !emailAddress && !message && !validateEmailAddress){
+      letsGetStarted();
+    }
+  });
 });
